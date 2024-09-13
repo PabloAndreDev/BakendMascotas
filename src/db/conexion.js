ba808256c1a3b40842936820462d2200007fde86
PabloAndreDev <pablogarcial4112@gmail.com>
2024-09-13 15:12:00 -0500

    eso

--- src/db/conexion.js
+++ src/db/conexion.js
@@ -10,11 +10,11 @@ dotenv.config({ path: path.resolve(__dirname, "../../env/.env") });
 
 
 export const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    host: 'mysql.railway.internal',
+    user: 'root',
+    password: 'RnvRxAVYexlHcSpzMCtlcHCOofjGOnUk',
+    port: '3306',
+    database: 'railway'
 })
 
 pool.getConnection().then(connect => {
